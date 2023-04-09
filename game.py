f8e44e1ec018094367864a7ad02f923bb419a4ef
Julio Amaya <julio@MacBook.local>
2023-04-09 15:15:26 -0500

    latest animation

--- game.py
+++ game.py
@@ -12,15 +12,16 @@ test_font = pygame.font.Font('fonts/speed-rush-font/SpeedRush-JRKVB.ttf', 50)
 
 
 background_surface = pygame.image.load('graphics/background01.png')
-text_surface = test_font.render('My Game', False, 'Green')
+text_surface = test_font.render('ZOOMY', False, 'Black')
+
+sprite_surface = pygame.image.load('graphics/arrow01.png')
+arrow_x_pos = 360
 
 
 
 
 # variable to set fps
 clock = pygame.time.Clock()
-
-
 
 
 #event loop
@@ -30,14 +31,15 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+
     # creating a simple surface
-
-
     screen.blit(background_surface,(0,0))
     screen.blit(text_surface,(300,50))
 
-
-
-    pygame.display.update() # update everything
+    arrow_x_pos += 4
+    screen.blit(sprite_surface,(arrow_x_pos,100))
+    pygame.display.update() 
     clock.tick(60) #limit frames to 60 fps
 
+    
