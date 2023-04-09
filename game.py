e48ce728639e52ff75545c9bc0ae846f31f011dc
Julio Amaya <julio@MacBook.local>
2023-04-09 11:34:08 -0500

    init

--- game.py
+++ game.py
@@ -1,1 +1,19 @@
 import pygame
+
+# initialize the game
+pygame.init()
+#size of the window size
+size = (400,400)
+screen = pygame.display.set_mode(size)
+#window heading
+pygame.display.set_caption("MAZZE")
+
+#event loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
+            
+
