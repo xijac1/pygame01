38c3232a21307805f585db7d4a0b06d972aed86b
Julio Amaya <julio@MacBook.local>
2023-04-09 14:12:56 -0500

    added font

--- game.py
+++ game.py
@@ -6,18 +6,22 @@ pygame.init()
 #size of the window size
 size = (800,400)
 #variables
-screen = pygame.display.set_mode(size)
-
-test_surface = pygame.image.load('graphics/background01.png')
-
-#set image dimensions
-#test_surface = pygame.transform.scale(test_surface, (800,400))
-
-#window heading
 pygame.display.set_caption("ZOOMY")
+screen = pygame.display.set_mode(size)
+test_font = pygame.font.Font('fonts/speed-rush-font/SpeedRush-JRKVB.ttf', 50)
+
+
+background_surface = pygame.image.load('graphics/background01.png')
+text_surface = test_font.render('My Game', False, 'Green')
+
+
+
 
 # variable to set fps
 clock = pygame.time.Clock()
+
+
+
 
 #event loop
 while True:
@@ -29,15 +33,11 @@ while True:
     # creating a simple surface
 
 
-    screen.blit(test_surface,(0,0))
+    screen.blit(background_surface,(0,0))
+    screen.blit(text_surface,(300,50))
 
 
 
     pygame.display.update() # update everything
     clock.tick(60) #limit frames to 60 fps
-
-
-
-
-
 
