eebdffa66dbcf320e5ce2149e3f495836bb1a29b
Julio Amaya <julio@MacBook.local>
2023-04-09 13:21:16 -0500

    added surface

--- game.py
+++ game.py
@@ -1,19 +1,41 @@
 import pygame
+from sys import exit
 
 # initialize the game
 pygame.init()
 #size of the window size
-size = (400,400)
+size = (800,400)
+#variables
 screen = pygame.display.set_mode(size)
+
+test_surface = pygame.image.load('graphics/background01.jpeg')
+
+#set image dimensions
+test_surface = pygame.transform.scale(test_surface, (800,400))
+
 #window heading
 pygame.display.set_caption("MAZZE")
 
+clock = pygame.time.Clock()
+
 #event loop
 while True:
+    #quit the game when window is closed
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            quit()
+            exit()
+    # creating a simple surface
 
-            
+
+    screen.blit(test_surface,(0,0))
+
+
+    pygame.display.update()     # update everything
+    clock.tick(60) #limit frames to 60 fps
+
+
+
+
+
 
