61343a710d8fe39a1992fcc16821a1f1f35623cc
julio <julio@MacBook.local>
2023-05-02 09:05:10 -0500

    added movement to sprite

--- game.py
+++ game.py
@@ -15,7 +15,12 @@ background_surface = pygame.image.load('graphics/background01.png')
 text_surface = test_font.render('ZOOMY', False, 'Black')
 
 sprite_surface = pygame.image.load('graphics/arrow01.png')
+
+#position
+arrow_y_pos = 100
 arrow_x_pos = 360
+vel = 4
+direction = "down"
 
 
 
@@ -31,15 +36,37 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+             # check for events
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP and direction != "down":
+                direction = "up"
+            elif event.key == pygame.K_DOWN and direction != "up":
+                direction = "down"
+            elif event.key == pygame.K_LEFT and direction != "right":
+                direction = "left"
+            elif event.key == pygame.K_RIGHT and direction != "left":
+                direction = "right"
+       
+
+    # move snake
+    if direction == "up":
+        arrow_y_pos -= vel
+    elif direction == "down":
+        arrow_y_pos += vel
+    elif direction == "left":
+        arrow_x_pos -= vel
+    elif direction == "right":
+        arrow_x_pos += vel
+
+
 
 
     # creating a simple surface
     screen.blit(background_surface,(0,0))
     screen.blit(text_surface,(300,50))
 
-    arrow_x_pos += 4
-    screen.blit(sprite_surface,(arrow_x_pos,100))
+    
+
+    screen.blit(sprite_surface,(arrow_x_pos,arrow_y_pos))
     pygame.display.update() 
     clock.tick(60) #limit frames to 60 fps
-
-    
